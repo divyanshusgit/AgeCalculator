8b9e3285a401d40857ff5ff0bd41063dfb21b94a
divyanshusgit <146812228+divyanshusgit@users.noreply.github.com>
2023-10-20 13:23:02 +0530

    final scripting

--- script.js
+++ script.js
@@ -1,210 +1,133 @@
-* {
-    font-family: sans-serif;
+let form = document.getElementById('form');
+setTimeout(() => {
+    form.style.opacity = '1'
+}, 1000);
+let inputElem = document.getElementById('dob');
+let submitBtn = document.getElementById('submit');
+let outputBox = document.getElementById('outputBox');
+let instructions = document.getElementById('instructions');
+let tryAgain = document.getElementById('tryAgain');
+tryAgain.addEventListener('click',reloadFunc);
+function reloadFunc(elem){
+    elem.target.style.outline = 'none';
+    setTimeout(() => {
+        elem.target.style.outline = '1px solid black';
+    }, 100);
+    setTimeout(() => {
+        outputBox.style.opacity = 0;
+        setTimeout(() => {
+            location.reload();
+        }, 500);
+    }, 200);
 }
 
-#container {
-    margin: auto;
-    width: 95vw;
-    height: 80vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+let today = new Date();
+if (today.getDate() < 10) {
+    inputElem.max = `${today.getFullYear()}-${today.getMonth() + 1}-0${today.getDate()}`;
 }
-
-#form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 15px;
-    opacity: 0;
-    transition: 1s all;
+else {
+    inputElem.max = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 }
+inputElem.value = inputElem.max;
 
-#dob {
-    text-align: center;
-    font-size: 40px;
-    border-radius: 2px;
-    padding: 10px;
-    width: 290px;
-    border: none;
-    display: inline-block;
-    margin: 10px 0px;
-    border-top: 30px solid rgb(255, 0, 0, 0.7);
-    background: linear-gradient(90deg, rgb(255, 255, 220) 0%, rgb(252, 251, 188) 100%);
-    outline: 1px solid black;
-    font-weight: 700;
-}
-
-#instructions {
-    margin: 10px 0px;
-    padding: 10px;
-    font-size: 20px;
-    color: rgb(130, 130, 130);
-    text-align: center;
-    transition: 0.5s all;
-}
-
-.buttons {
-    font-size: 20px;
-    border-radius: 2px;
-    padding: 10px;
-    border: none;
-    outline: 1px solid black;
-    background: linear-gradient(90deg, rgb(255, 255, 220) 0%, rgb(252, 251, 188) 100%);
-}
-
-#tryAgain {
-    text-align: center;
-    width: 155px;
-    margin: 10px auto;
-}
-
-.buttons:hover{
-    cursor: pointer;
-}
-
-#outputBox {
-    display: none;
-    opacity: 0;
-    transition: 1s all;
-}
-
-#outputData {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    transition: 1s all;
-}
-
-.output-elems {
-    padding: 10px 10px;
-}
-
-.innerOnes {
-    border-radius: 1px;
-    text-align: center;
-    margin: 10px;
-    padding: 10px;
-    color: black;
-    width: 70px;
-}
-
-.nums {
-    border-top: 20px solid red;
-    font-size: 50px;
-    background: linear-gradient(90deg, rgb(255, 255, 220) 0%, rgb(252, 251, 188) 100%);
-    outline: 1px solid black;
-    font-weight: 700;
-}
-
-.texts {
-    text-align: center;
-    color: rgb(130, 130, 130);
-}
-
-input[type="date"]::-webkit-calendar-picker-indicator {
-    background-image: url('calendar.svg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 26px;
-    position: relative;
-    right: 13px;
-    bottom: 2px;
-}
-
-input[type="date"]::-date-picker-indicator {
-    background-image: url('calendar.svg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 26px;
-    position: relative;
-    right: 13px;
-    bottom: 2px;
-}
-
-@media(max-width:630px) {
-    #dob {
-        font-size: 30px;
-        width: 220px;
-        border-top-width: 25px;
+submitBtn.addEventListener('click', submitClickFunc);
+function submitClickFunc(elem) {
+    elem.target.setAttribute('style', 'outline: 1px solid white;')
+    setTimeout(() => {
+        elem.target.setAttribute('style', 'outline: 1px solid black;')
+    }, 100);
+    if (new Date(inputElem.value) <= new Date(inputElem.max)) {
+        if (inputElem.value == '') {
+            elem.preventDefault();
+        }
+        else {
+            setTimeout(() => {
+                form.style.opacity = '0';
+                setTimeout(() => {
+                    form.style.display = 'none';
+                    logic();
+                    outputBox.setAttribute('style', 'display:flex; flex-direction: column;')
+                    setTimeout(() => {
+                        outputBox.style.opacity = '1';
+                    }, 400);
+                }, 900);
+            }, 200);
+        }
     }
-
-    #instructions {
-        font-size: 20px;
-    }
-
-    .buttons {
-        font-size: 16px;
-        padding: 4px 8px;
-    }
-
-    .nums {
-        border-top-width: 18px;
-        font-size: 35px;
-    }
-
-    #tryAgain {
-        width: 125px;
-    }
-
-    .texts {
-        font-size: 16px;
-    }
-
-    .output-elems {
-        padding: 0px;
-    }
-
-    .innerOnes {
-        width: 55px;
-    }
-
-    input[type="date"]::-webkit-calendar-picker-indicator {
-        background-size: 20px;
-        right: 10px;
+    else{
+        instructions.style.opacity = '0'
+        setTimeout(() => {
+            instructions.innerHTML = 'Enter a valid DOB'
+            instructions.style.opacity = '1'
+        }, 700);
     }
 }
-
-@media(max-width:530px) {
-    #dob {
-        font-size: 20px;
-        width: 150px;
-        border-top-width: 18px;
+function logic() {
+    let yearNum = document.getElementById('yearNum');
+    let monthNum = document.getElementById('monthNum');
+    let dayNum = document.getElementById('dayNum');
+    let today = new Date();
+    let dob = new Date(inputElem.value);
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth();
+    let currentDate = today.getDate();
+    let dobYear = dob.getFullYear();
+    let dobMonth = dob.getMonth();
+    let dobDate = dob.getDate();
+    let years;
+    let months;
+    let days;
+    if (currentMonth > dobMonth) {
+        years = currentYear - dobYear;
+        if (currentDate >= dobDate) {
+            months = currentMonth - dobMonth;
+            days = currentDate - dobDate;
+        }
+        else {
+            months = currentMonth - dobMonth - 1;
+            days = 30 - (dobDate - currentDate);
+        }
     }
-
-    input[type="date"]::-webkit-calendar-picker-indicator {
-        background-size: 15px;
-        right: 7px;
-        bottom: 1px;
+    else if (currentMonth < dobMonth) {
+        years = currentYear - dobYear - 1;
+        if (currentDate >= dobDate) {
+            months = 12 - (dobMonth - currentMonth);
+            days = currentDate - dobDate;
+        }
+        else {
+            months = 12 - (dobMonth - currentMonth) - 1;
+            days = 30 - (dobDate - currentDate);
+        }
     }
-}
-
-@media (max-width:340px) {
-    .nums {
-        border-top-width: 15px;
-        font-size: 30px;
+    else {
+        if (currentDate >= dobDate) {
+            years = currentYear - dobYear;
+            months = 0;
+            days = currentDate - dobDate;
+        }
+        else {
+            years = currentYear - dobYear - 1;
+            months = 11;
+            days = 30 - (dobDate - currentDate);
+        }
     }
-
-    .texts {
-        font-weight: normal;
-        text-align: center;
+    let arr = [0, 1, 3, 5, 7, 8, 10]
+    if (arr.includes(currentMonth)) {
+        dayNum.innerHTML = days + 1;
     }
-
-    .innerOnes {
-        padding: 5px;
-        width: 50px;
+    else {
+        if (currentMonth == 2) {
+            if (currentYear % 4 == 0) {
+                dayNum.innerHTML = days - 1;
+            }
+            else {
+                dayNum.innerHTML = days - 2;
+            }
+        }
+        else {
+            dayNum.innerHTML = days;
+        }
     }
-
-}
-
-@media(max-width:260px) {
-    .innerOnes {
-        margin: 5px 0px;
-    }
-
-    #month {
-        margin: 5px 7px;
-    }
+    yearNum.innerHTML = years;
+    monthNum.innerHTML = months;
 }
